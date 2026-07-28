b892bf7821eacbbeca5a7f4f92f9bf3dc5bf8f70
Pari <sharmaparis04628@gmail.com>
2026-07-28 09:30:56 +0530

    day3

--- day3.js
+++ day3.js
@@ -1,2 +1,23 @@
 //promises for asynchronous
 //js single threaded programming language
+
+let msg = true;
+
+const promiseOne = new Promise((resolve, reject) => {
+
+    if (!msg == true) {
+        console.log("message using promises failed");
+        reject("Error");
+    } else {
+        console.log("error.........");
+
+        setTimeout(() => {
+            resolve("Promise resolved successfully");
+        }, 2000);
+    }
+
+});
+
+promiseOne.then((result) => {
+    console.log(result);
+});
