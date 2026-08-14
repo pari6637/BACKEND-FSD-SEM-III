692aada091d73211a03ec4e50c76e4db3b7fd81f
Pari <sharmaparis04628@gmail.com>
2026-08-14 13:05:12 +0530

    .......

---
+++ exp-1.js
@@ -0,0 +1,13 @@
+const EventEmitter=require('events');
+class MyEvent extends EventEmitter{
+
+}
+const events=new EventEmitter();
+events.on("greets",()=>{
+    
+    console.log(`hello cse24 my name is ${name}`);;//template liberals-${var}
+})
+events.on("exit",(name)=>{
+})
+events.emit("greet","pari");
+events.emit("exit");
