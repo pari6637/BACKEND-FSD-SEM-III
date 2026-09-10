e8f7fece28554cda9d5ba5dc41c2ab96dffbb426
Pari <sharmaparis04628@gmail.com>
2026-09-10 11:16:00 +0530

    ........

--- RESTAPI/Practice.js
+++ RESTAPI/Practice.js
@@ -5,6 +5,7 @@ let users=[
     { id: 1, name: "Nirjara", email: "nirjara@example.com" },
     { id: 2, name: "Pavni", email: "pavni@example.com" }
 ];
+
 //Get :get request to fetch all users
 app.get('/users',(req,res)=>{
     res.json(users);
@@ -18,7 +19,29 @@ const user={
 };
 users.push(user);
 res.json(user);
-})
+});
+//PUT:request to update a user
+app.put('/users/:id',(req,res)=>{
+    let user=users.find(u=>u.id==req.params.id);
+    user.name=req.body.name;
+    user.email=req.body.email;
+    res.send("user updated successfully");
+     
+    res.json(user);
+    //DELETE:request to delete a user
+    app.delete('/users/:id',(req,res)=>{
+        users=users.filter(u=>u.id!=req.params.id);
+        res.send("user deleted successfully");})
+});
+
 app.listen(8000,()=>{
     console.log('Server is running on port http://localhost:8000');
 });
+//Create a PRODUCT REST API and test all method in THUNDER CLIENT
+//work it on approx 100 products and test all the methods in THUNDER CLIENT
+//Structure
+//1. create folder productrestapi
+//2. create index.js file
+//3.create a product.json
+//4.install npm init :package.json
+//5.install express: npm i express :package_lock.json
